3deb8419f43a4be11281aede334f5f79a017e80f
japhetvance <jvmpamonag@gmail.com>
2024-09-18 03:30:44 +0800

    finish RPS game

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -18,4 +18,58 @@ function getHumanChoice(){
     return humanChoice;
 }
 
-console.log(getHumanChoice());
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice){
+    if(humanChoice === computerChoice){
+        return 'It\'s a tie!';
+    }
+    if(humanChoice === 'rock'){
+        if(computerChoice === 'paper'){
+            computerScore++;
+            return 'You lose! Paper beats rock.';
+        } else {
+            humanScore++;
+            return 'You win! Rock beats scissors.';
+        }
+    }
+    if(humanChoice === 'paper'){
+        if(computerChoice === 'scissors'){
+            computerScore++;
+            return 'You lose! Scissors beats paper.';
+        } else {
+            humanScore++;
+            return 'You win! Paper beats rock.';
+        }
+    }
+    if(humanChoice === 'scissors'){
+        if(computerChoice === 'rock'){
+            computerScore++;
+            return 'You lose! Rock beats scissors.';
+        } else {
+            humanScore++;
+            return 'You win! Scissors beats paper.';
+        }
+    }
+}
+
+function playGame(){
+    for(let i = 0; i < 5; i++){
+        let humanChoice = getHumanChoice();
+        let computerChoice = getComputerChoice();
+        console.log(playRound(humanChoice, computerChoice));
+    }
+    console.log('Game Over!');
+    if(humanScore > computerScore){
+        console.log('You win!');
+    } else if(humanScore < computerScore){
+        console.log('You lose!');
+    } else {
+        console.log('It\'s a tie!');
+    }
+    console.log('Human Score: ' + humanScore);
+    console.log('Computer Score: ' + computerScore);
+}
+
+playGame();
